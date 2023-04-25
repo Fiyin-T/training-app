c20eb1f9e65855a0ed1571b81f7c81e979bacb70
Fiyin <fiyin@MacBook.local>
2023-04-25 08:57:14 -0600

    Player training details

--- src/components/TrainingDetails/TrainingDetails.jsx
+++ src/components/TrainingDetails/TrainingDetails.jsx
@@ -1,9 +1,33 @@
 import './TrainingDetails.css'
 
-export default function TrainingDetails() {
+export default function TrainingDetails({playerTrainings}) {
 
     return (
-        <h1>Training Details</h1>
+        <main className='TrainingDetails'>
+            <h6>Training Details</h6>
+            <table className='training-table'>
+                <thead>
+                    <tr>
+                        <th>Attribute</th>
+                        <th>Activity</th>
+                        <th>Duration[hours]</th>
+                    </tr>
+                </thead>
+                <tbody> 
+                    {   playerTrainings.map((trainings) => {
+                        return (
+                            <tr key={trainings.attribute}>
+                                <td>{trainings.attribute}</td>
+                                <td>{trainings.activity}</td>
+                                <td>{trainings.duration}</td>
+                            </tr>
+                            )
+                        })
+                    } 
+                </tbody>
+            </table>
+        </main>
+
 
     )
 
