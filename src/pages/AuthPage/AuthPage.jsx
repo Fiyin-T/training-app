4d15cc5cf002853951dfa381a6e3e5b98b48f019
Fiyin <fiyin@MacBook.local>
2023-04-25 08:48:56 -0600

    Player Login

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,19 +2,20 @@ import { useState } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import UserLoginForm from '../../components/UserLoginForm/UserLoginForm';
 import PlayerLoginForm from '../../components/PlayerLoginForm/PlayerLoginForm';
+import './AuthPage.css'
 
 export default function AuthPage({ setUser, setPlayer }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
     <main>
-      <h1>AuthPage</h1>
+      <h4>AuthPage</h4>
       <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
       { showSignUp ?
           <SignUpForm setUser={setUser} />
           :
           <>
             <UserLoginForm setUser={setUser} />
-            <h2>Player Login</h2>
+            <h4>Player Login</h4>
             <PlayerLoginForm setPlayer={setPlayer} />
           </>
 
