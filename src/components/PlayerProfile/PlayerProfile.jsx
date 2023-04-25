24d672cedba00029e1ddd0458a57b425a26d6253
Fiyin <fiyin@MacBook.local>
2023-04-25 09:03:28 -0600

    PlayerProfile component - player details

--- src/components/PlayerProfile/PlayerProfile.jsx
+++ src/components/PlayerProfile/PlayerProfile.jsx
@@ -1,11 +1,20 @@
 import './PlayerProfile.css'
 
-export default function PlayerProfile( { PlayerProfile}) {
+export default function PlayerProfile( { playerProfile }) {
 
     return (
         <main className='PlayerProfile'>
-
+            <div className='card'>
+                <div className='card-content'>
+                    <h5 className="teal-text">{playerProfile.name}'s Profile</h5>
+                    <p>Name: {playerProfile.name} </p>
+                    <p>Email: {playerProfile.email} </p>
+                    <p>Age: {playerProfile.age} </p>
+                    <p>Position: {playerProfile.position} </p>
+                    <p>Height: {playerProfile.height} </p>
+                    <p>Preferred Foot: {playerProfile.preferredFoot} </p>
+                </div>
+            </div>
         </main>
-
     )
 }
