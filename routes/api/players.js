93fd0f4d5ccd01aef363e60d9448e44145587dcd
Fiyin <fiyin@MacBook.local>
2023-04-25 09:14:31 -0600

    routes - GET/POST player trainings

--- routes/api/players.js
+++ routes/api/players.js
@@ -5,11 +5,18 @@ const playersCtrl = require('../../controllers/api/players');
 
 // POST /api/players
 router.post('/', playersCtrl.create);
+
 // POST /api/players/login
 router.post('/login', playersCtrl.login);
 
+// POST /api/players/:id/trainings
+router.post('/:id/trainings', playersCtrl.addTraining)
+
 // GET /api/players/:id
 router.get('/:id', playersCtrl.show)
+
+// GET /api/players/trainings/:id
+router.get('/trainings/:id', playersCtrl.trainings)
 
 // DELETE /api/players/:id/delete
 router.delete('/:id/delete', playersCtrl.delete)
